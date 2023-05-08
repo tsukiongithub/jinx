af20c51986120cc5e69f8001bbf6b33c6d73d15d
tsukiongithub <lucasmoon0@gmail.com>
2023-05-08 18:33:38 +0200

    fixed bg img gradient

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import Header from "@/components/Header";
 import { type NextPage } from "next";
 import Head from "next/head";
 import ProjectGrid from "@/components/ProjectGrid";
-import ProficienciesGrid from "@/components/ProficienciesGrid";
 
 const Home: NextPage = () => {
 	return (
