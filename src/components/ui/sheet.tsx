f6c3623cbf39762c12cdbdcff7eadb1e66095b9b
tsuki <lucasmoon0@gmail.com>
2024-01-17 08:08:39 +0100

    fix close icon for nav menu on mobile

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -4,6 +4,8 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { X } from "lucide-react";
 
 import { cn } from "@/lib/utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 
 const Sheet = SheetPrimitive.Root;
 
@@ -15,7 +17,7 @@ const SheetPortal = SheetPrimitive.Portal;
 
 const SheetOverlay = React.forwardRef<React.ElementRef<typeof SheetPrimitive.Overlay>, React.ComponentPropsWithoutRef<typeof SheetPrimitive.Overlay>>(({ className, ...props }, ref) => (
 	<SheetPrimitive.Overlay
-		className={cn("data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0  fixed inset-0 z-50 bg-black/80", className)}
+		className={cn("fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0", className)}
 		{...props}
 		ref={ref}
 	/>
@@ -47,8 +49,11 @@ const SheetContent = React.forwardRef<React.ElementRef<typeof SheetPrimitive.Con
 			{...props}
 		>
 			{children}
-			<SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-neutral-950 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-neutral-300 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-neutral-800">
-				<X className="h-4 w-4" />
+			<SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm text-neutral-50 opacity-70 ring-offset-neutral-950 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-neutral-300 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-neutral-800">
+				<FontAwesomeIcon
+					className="h-4 w-4"
+					icon={faX}
+				/>
 				<span className="sr-only">Close</span>
 			</SheetPrimitive.Close>
 		</SheetPrimitive.Content>
