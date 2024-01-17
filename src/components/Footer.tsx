b606ba51274173e7f4e86915c89a3c567562ec7a
tsuki <lucasmoon0@gmail.com>
2024-01-17 18:22:42 +0100

    update links to be more mobile friendly

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,29 @@
+import { cn } from "@/lib/utils";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 const Footer = ({ className }: { className?: string }) => {
 	return (
-		<footer className={`${className ? className : ""} mt-auto flex flex-col items-center px-6 py-6`}>
-			<div className="mb-2 flex">
+		<footer className={cn(`mt-auto flex flex-col items-center gap-2 px-6 py-6`, className)}>
+			<div className="text-center">
+				<p>
+					Copyright © 2024 by{" "}
+					<Link
+						className="md:animated-underline underline underline-offset-4 md:no-underline"
+						href="https://twitter.com/tsukiisadev"
+						target="_blank"
+					>
+						tsuki
+					</Link>
+				</p>
+				<p>All Rights Reserved.</p>
+			</div>
+			<div className="mb-2 flex flex-col gap-2 whitespace-nowrap text-sm md:flex-row md:gap-0">
 				<div className="mr-2">
 					Built using{" "}
 					<Link
-						className="animated-underline after:bg-white"
+						className="md:animated-underline underline underline-offset-4 md:no-underline"
 						href="https://nextjs.org/"
 						target="_blank"
 					>
@@ -17,7 +31,7 @@ const Footer = ({ className }: { className?: string }) => {
 					</Link>{" "}
 					and{" "}
 					<Link
-						className="animated-underline after:bg-white"
+						className="md:animated-underline underline underline-offset-4 md:no-underline"
 						href="https://tailwindcss.com/"
 						target="_blank"
 					>
@@ -25,30 +39,24 @@ const Footer = ({ className }: { className?: string }) => {
 					</Link>
 					.
 				</div>
-				<Link
-					className="animated-underline flex items-center justify-center after:bg-white"
-					href="https://vercel.com/"
-					target="_blank"
-				>
-					Hosted with
-					<FontAwesomeIcon
-						className="mx-2 h-3 w-3"
-						icon={faHeart}
-					/>
-					on Vercel.
-				</Link>
+				<div className="flex items-center justify-center gap-1">
+					<span>
+						Hosted with{" "}
+						<FontAwesomeIcon
+							className="h-3 w-3"
+							icon={faHeart}
+						/>{" "}
+						on{" "}
+					</span>
+					<Link
+						className="md:animated-underline underline underline-offset-4 md:no-underline"
+						href="https://vercel.com/"
+						target="_blank"
+					>
+						Vercel.
+					</Link>
+				</div>
 			</div>
-			<p>
-				Copyright © 2024 by{" "}
-				<Link
-					className="animated-underline after:bg-white"
-					href="https://twitter.com/tsukiisadev"
-					target="_blank"
-				>
-					tsuki
-				</Link>
-			</p>
-			<p>All Rights Reserved.</p>
 		</footer>
 	);
 };
